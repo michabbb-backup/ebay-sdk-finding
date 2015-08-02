571c7c1234353d6b34680e735a6fc3e017669f31
David T. Sadler <davidtsadler@googlemail.com>
2015-08-02 08:58:46 +0100

    0.2.2 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # CHANGELOG
 
-## Unreleased
+## 0.2.2 - 2015-08-02
 
 ### END OF LIFE NOTICE
 
--- src/DTS/eBaySDK/Finding/Services/FindingBaseService.php
+++ src/DTS/eBaySDK/Finding/Services/FindingBaseService.php
@@ -25,7 +25,7 @@ class FindingBaseService extends \DTS\eBaySDK\Services\BaseService
     /**
      * @var string Current version of the SDK
      */
-    const VERSION = '0.2.1';
+    const VERSION = '0.2.2';
 
     /**
      * Constants for the various HTTP headers required by the API.
